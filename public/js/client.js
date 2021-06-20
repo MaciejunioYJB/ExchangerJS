cc4f5fc9cfa277e5c14fd205162736abbb2185f0
MaciejunioYJB <masuper97@gmail.com>
2021-06-20 15:39:47 +0200

    crypto initial implementation added

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,5 @@
 var file = null;
+var cryptoFile = null;
 /*
 GET OPERATION
 var headers = new Headers();
@@ -22,10 +23,14 @@ console.log(file);
 fetch('json/currencies.json')
   .then(response => response.json())
   .then(jsonResponse => file = jsonResponse);
+  
+fetch('json/crypto.json')
+.then(response => response.json())
+.then(jsonResponse => cryptoFile = jsonResponse);
 
 function submit() {
-    const baseCurrency = document.getElementById("cf-from").value;
-    const currencyToConvert = document.getElementById("cf-to").value;
+    const baseCurrency = document.getElementById("cf-from").value.toUpperCase();
+    const currencyToConvert = document.getElementById("cf-to").value.toUpperCase();
     const valueToConvert = document.getElementById("cf-value").value;
     const resultBox = document.getElementById("result");
     const errorBox = document.getElementById("error");
@@ -37,11 +42,21 @@ function submit() {
     convert(file, baseCurrency, currencyToConvert, valueToConvert, resultBox, errorBox);
 }
 
+function submitCrypto() {
+    const cryptoCurrency = document.getElementById("cf-from2").value.toUpperCase();
+    const resultBox = document.getElementById("result-crypto");
+    const errorBox = document.getElementById("error-crypto");
+    resultBox.innerText = "";
+    errorBox.innerText = "";
+    console.log("cryptoCurrency -> " + cryptoCurrency);
+    convertCrypto(cryptoFile, cryptoCurrency, resultBox, errorBox);
+}
+
 function convert(response, baseCurrency, currencyToConvert, valueToConvert, resultBox, errorBox) {
     const convertedValue = parseInt(valueToConvert.replace(',','.'));
     found = false;
     response.forEach(currency => {
-        if(currency.base === baseCurrency) {
+        if (currency.base === baseCurrency) {
             console.log("BASE -> " + currency.base);
             var value = currency.rates[currencyToConvert];
             console.log("VALUE -> " + value);
@@ -57,4 +72,14 @@ function convert(response, baseCurrency, currencyToConvert, valueToConvert, resu
     if (!found) {
         errorBox.innerText = "Unable to found " + baseCurrency;
     }
+}
+
+function convertCrypto(response, cryptoCurrency, resultBox, errorBox) {
+    found = false;
+    response.forEach(currency => {
+        if (currency.symbol)
+    });
+    
+    resultBox.innerText = "result test";
+    errorBox.innerText = "error test";
 }
